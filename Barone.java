20c80886b176b42e5882d46b86667a50b5db9045
SIRISHAMLRS <51473411+SIRISHAMLRS@users.noreply.github.com>
2020-07-22 16:31:22 +0530

    Add files via upload

---
+++ Barone.java
@@ -0,0 +1,8 @@
+package com.epam.anjanisukhavasi_oops.chocolates;
+
+public class Barone extends Chocolate {
+
+    public Barone(String name, int price, int weight) {
+        super(name, price, weight);
+    }
+}
